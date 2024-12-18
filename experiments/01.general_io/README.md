2e79812f0e3c593132d006dbbf7da5474b096fcc
Alan Smewing <a.smewing@can-phantom.com>
2024-12-18 18:03:33 +0000

    Added code to demostrate using the pinmux and the PWM outputs.

--- experiments/01.general_io/README.md
+++ experiments/01.general_io/README.md
@@ -9,4 +9,7 @@ This code demostrates how to access the various hardware IO by:
 2. Falshing the on board LEDs
 3. Responding to the Joystick
 4. Switching GPIO27 of the Raspberry Pi header on and off at a slow rate to demostrate writing to the RPi header
-5. 
+5. GPIO12 of the Raspberry Pi header will periodically (every time the RGB LED goes dark purple) switch between 0% (off), 50% (1.5V) and 100% (3.3V). The demonstrate the use of the pinmux (pin multiplexor) and the PWM module.
+
+The pinmux is particularly odd because it doesn't use MMIO_CAPABILITY, insetad you just create a new instance of the class. However, the pinmux is due to be upgraded shortly.
+
--- experiments/01.general_io/general_io.cc
+++ experiments/01.general_io/general_io.cc
@@ -7,6 +7,8 @@
 #include <platform-rgbctrl.hh>
 #include <thread.h>
 #include <platform-gpio.hh>
+#include <platform-pinmux.hh>
+#include <platform-pwm.hh>
 #include "gpio_helper.hh"
 
 
@@ -37,7 +39,7 @@ void __cheri_compartment("main_comp") main_entry()
 	Debug::log("Sonata general IO tests");
 
 	uint8_t colours[] = {255, 0, 0};
-	auto  leds    = MMIO_CAPABILITY(SonataRgbLedController, rgbled);
+	auto leds = MMIO_CAPABILITY(SonataRgbLedController, rgbled);
 	leds->rgb(SonataRgbLed::Led0, colours[0], colours[1], colours[2]);
 	leds->update();
 	set_rgb_led_colour(leds, colours);
@@ -47,6 +49,27 @@ void __cheri_compartment("main_comp") main_entry()
 	uint32_t rpi_out_num = 27;
 	rpi_gpio()->set_output_enable(rpi_out_num, true);
 	rpi_gpio()->set_output(rpi_out_num, rp_out_state);
+
+	// Setting up the multiplexor to drive the PWM on RPi Header pin 12
+	// OutputPin::rph_g12 - The pin
+	Debug::log("RPi GPIO12 output pins has {} options.", SonataPinmux::output_pin_options(SonataPinmux::OutputPin::rph_g12));
+	// OutputPin::rph_g12 has 3 possible options and you can get them from looking at the table here: https://lowrisc.github.io/sonata-system/doc/ip/pinmux/index.html
+	// OutputPin::rph_g12 multiplexor options:
+	//   0 = Off, 
+	//   1 = Default RPi GPIO12 behaviour (gpio_0_ios_12),
+	//   2 = PWM output (pwm_out_1)
+	// The mutliplexor works in a different way to everything else in that is directly tied to it's memory in logic before you start! They did say that it would be changing at some stage.
+	if(true == (new SonataPinmux())->output_pin_select(SonataPinmux::OutputPin::rph_g12, 2)) {
+		Debug::log("Success! RPi GPIO12 set to PWM0");
+	} else {
+		Debug::log("ERROR! Failed to set RPi GPIO12 set to PWM0");
+	}
+	// Set PWM output 1 to 0%
+	uint8_t dutyCycle = 0;
+	uint32_t pwmOutput = 0;
+	Debug::log("PWM1 duty cycle {}/0xc8", dutyCycle);
+	MMIO_CAPABILITY(SonataPwm, pwm)->output_set(pwmOutput, 200,       dutyCycle); // Set duty cycle output to 50% (so should read 1.5V). Period and duty cycle are in clock cycles.
+	//                                   PWM output-^       ^-period   ^- duty cycle
 
 	uint8_t phase = 0;
 	uint8_t currentLed = 0;
@@ -79,6 +102,15 @@ void __cheri_compartment("main_comp") main_entry()
 				colours[0]++;
 				if(colours[2] == 0) {
 					phase = 0;
+
+					// Change PWM1's duty cycle
+					if(dutyCycle >= 200) {
+						dutyCycle = 0;
+					} else {
+						dutyCycle += 100;
+					}
+					Debug::log("PWM{} duty cycle {}/0xc8", pwmOutput, dutyCycle);
+					MMIO_CAPABILITY(SonataPwm, pwm)->output_set(pwmOutput, 200, dutyCycle); // Set duty cycle output.
 				}
 				break;
 		}
@@ -88,13 +120,15 @@ void __cheri_compartment("main_comp") main_entry()
 		currentLed++;
 		if(currentLed > gpio_data()->LEDCount) {
 			currentLed = 0;
+			
+			// Toggle RPi IO state
 			if(rp_out_state == true) {
 				rp_out_state = false;
 			} else {
 				rp_out_state = true;
 			}
 			rpi_gpio()->set_output(rpi_out_num, rp_out_state);
-			Debug::log("RPi GPIO{} {}", rpi_out_num, rp_out_state);
+			// Debug::log("RPi GPIO{} {}", rpi_out_num, rp_out_state);
 		}
 		gpio_data()->led_on(currentLed);
 
